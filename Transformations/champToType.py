799ba3d5ee37f19c7e1d4ad6c8ca0b4399a8c2a0
Hugo Teixeira <hugosteixeira@hotmail.com>
2019-05-17 02:33:50 -0300

    trying map

--- Transformations/champToType.py
+++ Transformations/champToType.py
@@ -1,27 +1,31 @@
 import pandas as pd
 #from Datasets.Kaggle.datasets import LeagueofLegends_df
 import json
-
-def changeValues(index,data):
-    print(index)
-    df.set_value(index, 'CHAMP1', lolChamps[champ1]['tags'])
-    df.set_value(index, 'CHAMP2', lolChamps[champ2]['tags'])
-
-def createDataFrames():
+class ChampTags:
+    def __init__(self):
+        self.lol_df = pd.read_csv('..\Results\selected_duos.csv')
     
-    lol_df = pd.read_csv('..\Results\selected_duos.csv')
-    lolChamps = json.loads(open('..\Datasets\league of legends\champion.json' , encoding="utf8").read())
-    part_df=lol_df.loc[:,['CHAMP1','CHAMP2','Total_Played','Wins']]
-    return1=part_df.rename(index=str,   columns={'CHAMP1':'TAG1',"CHAMP2":'TAG2'})
-    part1_df=return1.groupby(['TAG1','TAG2'],0,None,True,False,False).sum()
-    print(part1_df.iterrows().data)
-    map(changeValues,part1_df.iterrows())
-    return part1_df
+    def changeValues(self,index):
+        print('oi')
+        champ1 = df.get_value(index,'TAG1')
+        champ2 = df.get_value(index,'TAG2')
+        self.lol_df.set_value(index, 'TAG1', lolChamps[champ1]['tags'])
+        self.lol_df.set_value(index, 'TAG2', lolChamps[champ2]['tags'])
+
+    def createDataFrames(self,):
+        lolChamps = json.loads(open('..\Datasets\league of legends\champion.json' , encoding="utf8").read())
+        part_df=self.lol_df.loc[:,['CHAMP1','CHAMP2','Total_Played','Wins']]
+        return1=part_df.rename(index=str,   columns={'CHAMP1':'TAG1',"CHAMP2":'TAG2'})
+        self.lol_df=return1.groupby(['TAG1','TAG2'],0,None,True,False,False).sum()
+        lista = list(range(len(self.lol_df)))
+        print(lista)
+        map(self.changeValues,lista)
+        return self.lol_df
 
 
 
     
-
-createDataFrames().to_csv('..\Results\TAGS.csv', encoding='utf-8')
+ct=ChampTags()
+ct.createDataFrames().to_csv('..\Results\TAGS.csv', encoding='utf-8')
 
 
