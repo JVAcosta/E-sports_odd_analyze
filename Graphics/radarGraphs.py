33faba1ff2cb49e6bd7f1485d2e9a7ddc9db1708
Hugo Teixeira <hugosteixeira@hotmail.com>
2019-07-11 23:35:57 -0300

    hugo

--- Graphics/radarGraphs.py
+++ Graphics/radarGraphs.py
@@ -6,18 +6,11 @@ import matplotlib.pyplot as plt
 import math
 
 
-def f2(seq):
-    # order preserving
-    checked = []
-    for e in seq:
-        if e not in checked:
-            checked.append(e)
-    return checked
-
-
 def champToChamp():
     df = pd.read_csv('../Results/TAGS.csv')
-
+    df = df.loc[(df['TAG_Champ_1_Blue'] +
+                 df['TAG_Champ_2_blue'] == 'MarksmanSupport, Tank') | (df['TAG_Champ_1_Blue'] +
+                                                                       df['TAG_Champ_2_blue'] == 'MarksmanSupport, Mage')]
     df['CHAMP_DUOS_BLUE'] = df.iloc[0:-1, 1]+'-'+df.iloc[0:-1, 2]
     df['CHAMP_DUOS_RED'] = df.iloc[0:-1, 3]+'-'+df.iloc[0:-1, 4]
     df['RESULT(%)'] = df.iloc[0:-1, 5]/df.iloc[0:-1, 7]*100
@@ -34,7 +27,9 @@ def champToChamp():
     df.to_csv('teste_felipe.csv', encoding='utf-8')
 
     # number of variable
-    categories = list(df)[1:]
+    print(df)
+    categories = range(1, len(df.columns))
+    print(categories)
     N = len(categories)
     ax = plt.subplot(111, polar=True)
     ax.set_theta_offset(pi / 2)
@@ -45,21 +40,22 @@ def champToChamp():
     angles = np.concatenate((angles, [angles[0]]))
     plt.xticks(angles[:-1], categories)
     ax.set_rlabel_position(0)
-    plt.yticks([30, 50, 70], ["30", "50", "70"], color="grey", size=7)
+    plt.yticks([10, 20, 30, 40, 50, 60, 70, 80, 90, 100], ["10", "20", "30", "40", "50", '60',
+                                                           "70", "80", "90", '100'], color="grey", size=7)
     plt.ylim(0, 100)
-    values = df.iloc[0].values.tolist()
+    values = df.loc["Marksman-Support, Tank", :].values.tolist()
     values = [0 if math.isnan(x) else x for x in values]
-    ax.plot(angles, values, linewidth=1, linestyle='solid', label="group A")
+    ax.plot(angles, values, linewidth=1,
+            linestyle='solid', label="group A")
     ax.fill(angles, values, 'b', alpha=0.1)
-    values = df.iloc[3].values.tolist()
+
+    values = df.loc["Marksman-Support, Mage", :].values.tolist()
     values = [0 if math.isnan(x) else x for x in values]
-    ax.plot(angles, values, linewidth=1, linestyle='solid', label="group B")
+    ax.plot(angles, values, linewidth=1,
+            linestyle='solid', label="group b")
     ax.fill(angles, values, 'r', alpha=0.1)
-    values = df.iloc[5].values.tolist()
-    values = [0 if math.isnan(x) else x for x in values]
-    ax.plot(angles, values, linewidth=1, linestyle='solid', label="group C")
-    ax.fill(angles, values, 'g', alpha=0.1)
-    plt.legend(loc='upper right', bbox_to_anchor=(0.1, 0.1))
+
+    ax.legend((1, 2, 3), ('a', 'b', 'c'))
     plt.show()
 
 
